90e2edff92d608695e60b932861720c27f619d92
Harold Sikkema <harold@nsitu.ca>
2020-10-27 16:11:46 -0400

    Add code files

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+
+$hostname = "localhost";
+$username = "ixd1734_world";
+$password = "uZPM[7vbqjk5";
+$database = "ixd1734_imdb";
+
+// Create connection
+$db = new mysqli($hostname, $username, $password, $database);
+$db->set_charset("utf8");
+
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php require 'connect.php'; ?>
+<!doctype html>
+<html lang="en">
+<meta charset="UTF-8">
+<head>
+  <title>Bad Old Movies</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <h1>Terrible Films</h1>
+  <p>Some Really bad movies from a while backkkkk.</p>
+
+<?php
+
+$sql = "SELECT * FROM `movies` WHERE year = 1976 and rank < 4 ORDER BY `movies`.`rank` ASC LIMIT 10";
+$result = $db->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_array()) {
+    $exp = $row["rank"];
+    $name = $row["name"];
+    echo
+    '<div class="country">'.
+      '<div class="indicator" style="flex-basis: '.($exp*10).'%" >'.
+            $exp.
+      '</div>'.
+      '<div class="name">'.$name.'</div>'.
+    '</div>';
+  }
+} else {
+    echo "<p>Nothing to show</p>";
+}
+
+$db->close();
+?>
+
+</body>
+</html>
